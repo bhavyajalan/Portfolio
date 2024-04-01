3d6fbb374b0a9d46a36776075767c191475274ff
bhavyajalan <bhavyabansal1758@gmail.com>
2024-04-02 00:43:18 +0530

    Adding files

---
+++ src/constants/index.js
@@ -0,0 +1,270 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  pytorch,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+  IGTS,
+  supermario,
+  fashion,
+  bert,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Machine Learning Engineer",
+    icon: web,
+  },
+  {
+    title: "React Js Developer",
+    icon: mobile,
+  },
+  {
+    title: "Data Analyst",
+    icon: backend,
+  },
+  {
+    title: "MERN Stack Developer",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: pytorch,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "React.js Developer",
+    company_name: "The Indian Game Theory Society",
+    icon: IGTS,
+    iconBg: "white",
+    date: "March 2022 - Present",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies(Tailwind CSS).",
+      "Collaborating with cross-functional teams including designers,and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  // {
+  //   title: "React Native Developer",
+  //   company_name: "Tesla",
+  //   icon: tesla,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2021 - Feb 2022",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Web Developer",
+  //   company_name: "Shopify",
+  //   icon: shopify,
+  //   iconBg: "#383E56",
+  //   date: "Jan 2022 - Jan 2023",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+  // {
+  //   title: "Full stack Developer",
+  //   company_name: "Meta",
+  //   icon: meta,
+  //   iconBg: "#E6DEDD",
+  //   date: "Jan 2023 - Present",
+  //   points: [
+  //     "Developing and maintaining web applications using React.js and other related technologies.",
+  //     "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+  //     "Implementing responsive design and ensuring cross-browser compatibility.",
+  //     "Participating in code reviews and providing constructive feedback to other developers.",
+  //   ],
+  // },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+    name: "Sara Lee",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "I've never met a web developer who truly cares about their clients' success like Rick does.",
+    name: "Chris Brown",
+    designation: "COO",
+    company: "DEF Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Lisa Wang",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
+
+const projects = [
+  {
+    name: "Deep Reinforcement Learning Super Mario Run",
+    description:
+      "Trained a Super Mario to complete various levels, for 172 iterations. Ouptut - Mario was able to complete 1st level in record time.",
+    tags: [
+      {
+        name: "PyTorch",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "OpenAI Gym",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Weight n Biases",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: supermario,
+    source_code_link:
+      "https://github.com/samyak1512/Super-Mario-Reinforcement-Learning",
+  },
+  {
+    name: "Generative adversarial networks Fashion MNIST",
+    description:
+      " A GAN architecture that generates images of fashion items in a 28*28 image.",
+    tags: [
+      {
+        name: "Tensorflow",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Tensorboard",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Google Fashion MNIST Dataset",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: fashion,
+    source_code_link: "https://github.com/samyak1512/GAN-Fashion-Mnist",
+  },
+  {
+    name: "Classification of Tweets",
+    description:
+      "Implemented BERT with CNN to build a classifier for Tweets.Tweets related to abortion rights and Roe vs Wade were used as a dataset.Classified the tweets as Positive, Neutral, or Negative tweets.",
+    tags: [
+      {
+        name: "PyTorch",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "Hugging Face Trainer",
+        color: "green-text-gradient",
+      },
+      {
+        name: "Matplotlib",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: bert,
+    source_code_link: "https://www.kaggle.com/code/samyak15jain/bert-model",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
